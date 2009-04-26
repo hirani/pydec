55b509ed58046dc871dd8e62109b761cd2179ef7
wnbell <wnbell@dc9e490a-282d-0410-9628-ebb8bc68cd00>
2009-04-26 08:21:00 +0000

    cleaned up rips complex code

--- trunk/pydec/dec/rips_complex.py
+++ trunk/pydec/dec/rips_complex.py
@@ -8,30 +8,80 @@ from pydec.util import flatten
 from simplex_array import simplex_array_searchsorted
  
 class rips_complex:
-    def __init__(self, vertices, r):
+    def __init__(self, vertices, delta):
+        """Construct a Rips complex
+
+        Construct a Rips for an array of vertices and a radius delta.
+
+        Parameters
+        ----------
+        vertices : array_like
+            An N-by-D array of vertex coordinates where N is the 
+            number of vertices and D is the dimension of the space
+        delta : float
+            Radius used to determine the connectivity of the Rips complex.
+            Vertices which are separated by no more than distance delta
+            are connected by an edge in the 1-skeleton of the Rips complex.
+
+        Examples
+        --------
+        >>> from pydec.dec import rips_complex
+        >>> from numpy import array
+        >>> vertices = array([[0,0],[2,0],[2,2],[0,2],[1,1]], dtype='float')
+        >>> delta = 2.1
+        >>> rc = rips_complex(vertices, delta)
+        >>> print rc.simplices[0]
+        [[0]
+         [1]
+         [2]
+         [3]
+         [4]]
+        >>> print rc.simplices[1]
+        [[0 1]
+         [0 3]
+         [0 4]
+         [1 2]
+         [1 4]
+         [2 3]
+         [2 4]
+         [3 4]]
+        >>> print rc.simplices[2]
+        [[0 1 4]
+         [0 3 4]
+         [1 2 4]
+         [2 3 4]]
+
+        """
+
         vertices = asarray(vertices)
         
+        # construct kD-Tree for spatial queries
         tree = kd_tree(vertices)
 
-        L = [tree.in_sphere(v,r) for v in vertices]
+        # for each vertex, compute all vertices within distance r
+        L = [tree.in_sphere(v, delta) for v in vertices]
 
+        # convert list of list structure into array of edges
         i = arange(len(vertices)).repeat( [len(x) for x in L] )
         j = array( flatten(L) )
         edges = hstack((i.reshape(-1,1),j.reshape(-1,1)))
 
-        simplices = rips_simplices(edges, num_nodes=vertices.shape[0], \
-                k=vertices.shape[1])
+        # compute simplices of the Rips complex from edges (1-skeleton of Rips complex)
+        simplices = rips_simplices(vertices.shape[0], edges, vertices.shape[1])
 
+        # compute boundary operators of the Rips complex
         chain_complex = rips_chain_complex(simplices)
 
-
-        self.vertices = vertices
-        self.simplices = simplices
-        self._chain_complex = chain_complex
-
+        # store the cochain complex
         Bn = chain_complex[-1]
-        self._cochain_complex  = [ B.T for B in chain_complex[1:] ]
-        self._cochain_complex += [ csr_matrix( (1, Bn.shape[1]), dtype=Bn.dtype) ] 
+        cochain_complex  = [ B.T for B in chain_complex[1:] ]
+        cochain_complex += [ csr_matrix( (1, Bn.shape[1]), dtype=Bn.dtype) ] 
+        
+        # store the data members
+        self.vertices  = vertices
+        self.simplices = simplices
+        self._chain_complex   = chain_complex
+        self._cochain_complex = cochain_complex
     
     def complex_dimension(self):
         return len(self.cochain_complex()) - 1
@@ -50,9 +100,31 @@ class rips_complex:
         
 
 def rips_chain_complex(simplices):
+    """Construct the boundary operators for the Rips complex 
+
+    Constructs the boundary operators for the Rips complex for
+    a given a list of simplex arrays.
+
+    Parameters
+    ----------
+    simplices : list of arrays
+        List of length D, where simplices[i] is the simplex
+        array representing the i-dimensional simplices.
+
+    Returns
+    -------
+    Bs : list of sparse matrices
+        List of length D + 1 where Bs[i] is the boundary operator
+        for the i-dimensional simplices.
+
+    Examples
+    --------
+    TODO
+
+    """
     Bs = []
 
-    dtype = 'int8'
+    dtype = 'float32'
 
     simplices = [asarray(x) for x in simplices]
 
@@ -92,41 +164,79 @@ def rips_chain_complex(simplices):
 
 
 
-def rips_simplices(edges,num_nodes=None,k=2):
-    edges = asarray(edges,dtype='int32')
+def rips_simplices(num_vertices, edges, k):
+    """Compute simplices of the Rips complex
 
-    if num_nodes is None:
-        #infer from edge array
-        num_nodes = edges.max() + 1
+    Returns a list of simplex arrays representing
+    the 0 to k-dimensional simplices of the Rips complex.
+
+    Parameters
+    ----------
+    num_verticles : integer
+        Number of vertices/points/nodes in the Rips complex
+    edges : array_like
+        An N-by-2 array containing the edges of the Rips complex
+    k : integer
+        Maximum simplex dimension
+
+    Returns
+    -------
+    simplices : list of arrays
+        List of length k+1, where simplices[i] is the simplex
+        array representing the i-dimensional simplices.
+
+    Examples
+    --------
+    >>> from numpy import array
+    >>> edges = array([[0,1],[0,2],[1,0],[1,2],[2,0],[2,1]])
+    >>> simplices = rips_simplices(3, edges, 2)
+    >>> print simplices[0]
+    [[0]
+     [1]
+     [2]]
+    >>> print simplices[1]
+    [[0 1]
+     [0 2]
+     [1 2]]
+    >>> print simplices[2]
+    [[0 1 2]]
+
+    """
+
+    edges = asarray(edges, dtype='int32')
 
     simplices = []
         
     if len(edges.shape) != 2 and edges.shape[1] != 2:
-        raise ValueError,'expected N by 2 array for argument edges'
+        raise ValueError('expected N by 2 array for argument edges')
 
     # node simplices 
-    simplices.append( arange( num_nodes, dtype=edges.dtype).reshape(-1,1) )
+    simplices.append( arange(num_vertices, dtype=edges.dtype).reshape(-1,1) )
 
-    if k == 0 or len(edges) == 0: return simplices
+    # no work to do
+    if k == 0: return simplices
     
     edges = edges[edges[:,0] < edges[:,1]]   # orient edges 
-    edges = edges[lexsort( edges.T[::-1] )]  # sort edges 
+    if edges.shape[0] != 0:
+        edges = edges[lexsort( edges.T[::-1] )]  # sort edges 
+        simplices.append( edges )
 
-    simplices.append( edges )
-
-    if k == 1: return simplices
+    if k == 1 or edges.shape[0] == 0: return simplices
 
     # E[i,j] == 1 iff (i,j) is an edge and i < j
-    E = csr_matrix( (ones(len(edges),dtype='int8'), edges.T),shape=(num_nodes,num_nodes))
+    E = csr_matrix( (ones(len(edges), dtype='int32'), edges.T), shape=(num_vertices,num_vertices))
 
     k_faces = edges
 
     for n in range(1,k):
+        if k_faces.shape[0] == 0:
+            break
+
         indptr  = arange(0, (n+1) * (len(k_faces)+1), (n+1) )
         indices = k_faces.ravel()
-        data    = ones( len(indices), dtype='int8')
+        data    = ones(len(indices), dtype='int32')
           
-        F = csr_matrix((data,indices,indptr),shape=(len(k_faces),num_nodes))
+        F = csr_matrix((data, indices, indptr), shape=(len(k_faces), num_vertices))
 
         FE = F*E
         FE.data[FE.data != n+1] = 0
@@ -134,7 +244,7 @@ def rips_simplices(edges,num_nodes=None,k=2):
         FE.sort_indices()
 
         row = FE.tocoo(copy=False).row
-        k_faces = hstack((k_faces[row],FE.indices.reshape(-1,1)))
+        k_faces = hstack( (k_faces[row],FE.indices.reshape(-1,1)) )
 
         if len(k_faces) == 0:
             return simplices
@@ -142,6 +252,4 @@ def rips_simplices(edges,num_nodes=None,k=2):
         simplices.append( k_faces )
 
     return simplices
-
-
 
