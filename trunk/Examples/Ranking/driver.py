18ad4b91a9a5c4f222a2469077daf936abcba37c
anil.hirani <anil.hirani@dc9e490a-282d-0410-9628-ebb8bc68cd00>
2011-03-13 23:22:31 +0000

    added Ranking example

---
+++ trunk/Examples/Ranking/driver.py
@@ -0,0 +1,37 @@
+"""
+Least squares ranking on graphs.
+
+Reference :
+
+Least Squares Ranking on Graphs, Hodge Laplacians, Time Optimality,
+and Iterative Methods
+A. N. Hirani, K. Kalyanaraman, S. Watts
+See arXiv:1011.1716v1 [cs.NA] on http://arxiv.org/abs/1011.1716
+
+"""
+from numpy import loadtxt
+from pydec import abstract_simplicial_complex
+from scipy.sparse.linalg import lsqr
+
+# Load graph and edge values
+data = loadtxt('data.txt').astype(int)
+edges = data[:,:2]
+
+# Create abstract simplicial complex from edges
+asc = abstract_simplicial_complex([edges])
+
+omega = data[:,-1] # pairwise comparisons
+B1 = asc.chain_complex()[1] # boundary matrix
+alpha = lsqr(B1.T, omega)[0] # solve least squares problem
+
+# Scale minimum to 0 and maximum to 100
+alpha = alpha - alpha.min()
+alpha = alpha / alpha.max() * 100
+
+print alpha
+
+
+
+
+
+
