aa74d449b2d76156670671f474bd4ac55903f53b
wnbell <wnbell@dc9e490a-282d-0410-9628-ebb8bc68cd00>
2012-02-20 23:43:05 +0000

    use matplotlib's triplot instead of pydec's
    Fixes issue #2

--- trunk/Examples/MakeMesh/make_mesh.py
+++ trunk/Examples/MakeMesh/make_mesh.py
@@ -3,15 +3,15 @@ Reads ascii vertex and element files, writes a pydec mesh and displays it
 """
 
 import scipy
-from pydec import *
-from matplotlib.pylab import show
+from pydec import SimplicialMesh, write_mesh, read_mesh
+from matplotlib.pylab import triplot, show
 
 vertices = scipy.loadtxt("v.txt")
 elements = scipy.loadtxt("s.txt",dtype='int32') - 1
 mymesh = SimplicialMesh(vertices=vertices,indices=elements)
 write_mesh("square_8.xml",mymesh,format='basic')
 rmesh = read_mesh("square_8.xml")
-triplot(rmesh.vertices,rmesh.indices)
+triplot(rmesh.vertices[:,0], rmesh.vertices[:,1], rmesh.indices)
 
 show()
 
